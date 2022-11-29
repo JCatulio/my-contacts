5bc02db8df9ce3e22f724878a1e061baeb92a640
Jonathan <82682806+JCatulio@users.noreply.github.com>
2022-11-29 16:40:30 -0300

    Criando a listagem de contatos

--- src/assets/styles/themes/default.js
+++ src/assets/styles/themes/default.js
@@ -1,11 +1,15 @@
 export default {
   colors: {
-    background:'#f6f5fc',
+    background: '#f6f5fc',
     primary: {
       lighter: '#e0e3ff',
       light: '#6674f4',
       main: '#5061FC',
       dark: '#3346f0',
-    }
-  }
+    },
+    gray: {
+      900: '#222222',
+      200: '#bcbcbc',
+    },
+  },
 };
