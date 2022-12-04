c9b896bf15f7fa523af9502707bd3821f2ff5a77
Jonathan <82682806+JCatulio@users.noreply.github.com>
2022-12-04 20:22:52 -0300

    Exibindo mensagem de nenhum contato encontrado

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,6 +10,7 @@ import {
   Card,
   ErrorContainer,
   EmptyListContainer,
+  SearchNotFoundContainer,
 } from '../../pages/Home/styles';
 
 import arrow from '../../assets/images/icons/arrow.svg';
@@ -17,6 +18,7 @@ import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 import sad from '../../assets/images/sad.svg';
 import emptyBox from '../../assets/images/empty-box.svg';
+import magnifierQuestion from '../../assets/images/magnifier-question.svg';
 
 import Loader from '../../components/Loader';
 import Button from '../../components/Button';
@@ -125,6 +127,16 @@ export default function Home() {
                 seu primeiro!
               </p>
             </EmptyListContainer>
+          )}
+
+          {contacts.length > 0 && filteredContacts.length < 1 && (
+            <SearchNotFoundContainer>
+              <img src={magnifierQuestion} alt="Magnifier Question" />
+              <span>
+                Nenhum resultado foi encontrado para
+                <strong> {searchTerm}</strong>.
+              </span>
+            </SearchNotFoundContainer>
           )}
 
           {filteredContacts.length > 0 && (
