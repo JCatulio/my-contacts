ccb449ca3df31ec22468751b0333bcaaaf73bc58
Jonathan <82682806+JCatulio@users.noreply.github.com>
2022-12-11 15:13:37 -0300

    Capturando os dados do contato que está sendo deletado

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -32,6 +32,8 @@ export default function Home() {
   const [searchTerm, setSearchTerm] = useState('');
   const [isLoading, setIsLoading] = useState(true);
   const [hasError, setHasError] = useState(false);
+  const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(false);
+  const [contactBeingDeleted, setContactBeingDeleted] = useState(null);
 
   const filteredContacts = useMemo(
     () =>
@@ -72,23 +74,35 @@ export default function Home() {
     loadContacts();
   }
 
+  function handleDeleteContact(contact) {
+    setContactBeingDeleted(contact);
+    setIsDeleteModalVisible(true);
+  }
+
+  function handleCloseDeleteModal() {
+    setIsDeleteModalVisible(false);
+  }
+
+  function handleConfirmDeleteContact() {
+    console.log(contactBeingDeleted.id);
+  }
+
   return (
     <Container>
       <Loader isLoading={isLoading} />
 
-      <Modal
-        danger
-        title='Tem certeza que deseja remove o contato "Mateus Silva"?'
-        confirmLabel="Deletar"
-        onCancel={() => {
-          alert('Cancelou');
-        }}
-        onConfirm={() => {
-          alert('Confirmou');
-        }}
-      >
-        <h1>Oi</h1>
-      </Modal>
+      {isDeleteModalVisible && (
+        <Modal
+          danger
+          visible={isDeleteModalVisible}
+          title={`Tem certeza que deseja remover o contato "${contactBeingDeleted?.name}"?`}
+          confirmLabel="Deletar"
+          onCancel={handleCloseDeleteModal}
+          onConfirm={handleConfirmDeleteContact}
+        >
+          <p>Esta ação não poderá ser desfeita!</p>
+        </Modal>
+      )}
 
       {contacts.length > 0 && (
         <InputSearchContainer>
@@ -180,7 +194,10 @@ export default function Home() {
                 <Link to={`/edit/${contact.id}`}>
                   <img src={edit} alt="Edit" />
                 </Link>
-                <button type="button">
+                <button
+                  type="button"
+                  onClick={() => handleDeleteContact(contact)}
+                >
                   <img src={trash} alt="Delete" />
                 </button>
               </div>
