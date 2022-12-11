4b6c6eb9f916a4981824ae74f022082c2c5896e5
Jonathan <82682806+JCatulio@users.noreply.github.com>
2022-12-11 19:54:49 -0300

    Iniciando a criação do ContactMapper

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,24 +1,18 @@
+import { useRef } from 'react';
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
 import ContactsService from '../../services/ContactsService';
 import toast from '../../utils/toast';
-import { useRef } from 'react';
 
 export default function NewContact() {
   const contactFormRef = useRef(null);
 
-  async function handleSubmit(formData) {
+  async function handleSubmit(contact) {
     try {
-      const contact = {
-        name: formData.name,
-        email: formData.email,
-        phone: formData.phone,
-        category_id: formData.categoryId,
-      };
-
       await ContactsService.createContact(contact);
 
       contactFormRef.current.resetFields();
+
       toast({
         type: 'success',
         text: 'Contato cadastrado com sucesso!',
