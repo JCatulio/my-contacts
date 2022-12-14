e3354d50bb655113580fa443b14d4f71eace718f
Jonathan <82682806+JCatulio@users.noreply.github.com>
2022-12-14 20:50:12 -0300

    Implementando a animação de saída do Modal com timeout

--- src/components/Modal/styles.js
+++ src/components/Modal/styles.js
@@ -1,24 +1,24 @@
 /* eslint-disable indent */
-import styled, { keyframes } from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
 
 const fadeIn = keyframes`
-  from {
-    opacity: 0;
-  }
+  from { opacity: 0; }
+  to { opacity: 1; }
+`;
 
-  to {
-    opacity: 1;
-  }
+const fadeOut = keyframes`
+  from { opacity: 1; }
+  to { opacity: 0; }
 `;
 
 const scaleIn = keyframes`
-  from {
-    transform: scale(0);
-  }
+  from { transform: scale(0); }
+  to { transform: scale(1); }
+`;
 
-  to {
-    transform: scale(1);
-  }
+const scaleOut = keyframes`
+  from { transform: scale(1); }
+  to { transform: scale(0); }
 `;
 
 export const Overlay = styled.div`
@@ -33,6 +33,8 @@ export const Overlay = styled.div`
   align-items: center;
   justify-content: center;
   animation: ${fadeIn} 0.3s;
+
+  ${({isLeaving}) => isLeaving && css`animation: ${fadeOut} 0.2s;`}
 `;
 
 export const Container = styled.div`
@@ -43,6 +45,8 @@ export const Container = styled.div`
   padding: 24px;
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
   animation: ${scaleIn} 0.3s;
+
+  ${({isLeaving}) => isLeaving && css`animation: ${scaleOut} 0.2s;`}
 
   > h1 {
     font-size: 22px;
