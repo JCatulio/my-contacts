a9f683eb574ed0a830ee6e66c4b304637fc5c660
Jonathan <82682806+JCatulio@users.noreply.github.com>
2022-12-15 17:18:57 -0300

    Movendo a renderização da lista para uma função auxiliar

--- src/hooks/useAnimatedList.js
+++ src/hooks/useAnimatedList.js
@@ -2,28 +2,34 @@ import { useCallback, useState } from 'react';
 
 export default function useAnimatedList(initialValue = []) {
   const [items, setItems] = useState(initialValue);
-  const [pendingRemovalItemsIds, setPendingRemovalItemsIds] = useState(
-    [],
-  );
+  const [pendingRemovalItemsIds, setPendingRemovalItemsIds] = useState([]);
 
   const handleRemoveItem = useCallback((id) => {
     setPendingRemovalItemsIds((prevState) => [...prevState, id]);
   }, []);
 
   const handleAnimationEnd = useCallback((id) => {
-    setItems((prevState) =>
-      prevState.filter((item) => item.id !== id),
-    );
+    setItems((prevState) => prevState.filter((item) => item.id !== id));
     setPendingRemovalItemsIds((prevState) =>
       prevState.filter((itemId) => itemId !== id),
     );
   }, []);
 
+  const renderList = useCallback(
+    (renderItem) =>
+      items.map((item) => {
+        renderItem(item, {
+          isLeaving: pendingRemovalItemsIds.includes(item.id),
+        });
+      }),
+    [items, pendingRemovalItemsIds],
+  );
+
   return {
     items,
     setItems,
-    pendingRemovalItemsIds,
     handleRemoveItem,
     handleAnimationEnd,
+    renderList,
   };
 }
